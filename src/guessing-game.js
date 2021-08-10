9fbb0b2988f7aa0876cfb35437400831a80984fc
Oleksii Korovushkin <alexkrv07@gmail.com>
2021-08-10 22:43:27 +0300

    feat: solve task guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,26 @@
 class GuessingGame {
     constructor() {}
-
+        lowerBound;
+        upperBound;
+        result;
     setRange(min, max) {
-
+        this.lowerBound = min;
+        this.upperBound = max;
     }
 
     guess() {
-
+        this.result = Math.ceil((this.upperBound - this.lowerBound) / 2) + this.lowerBound;
+        return this.result;
     }
 
     lower() {
-
+        this.upperBound = this.result;
     }
 
     greater() {
-
+        this.lowerBound  = this.result;
     }
 }
 
 module.exports = GuessingGame;
+
